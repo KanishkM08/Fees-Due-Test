ee49a3c15a2296db057a1a13b12983c7bb8f8ef0
Kanishk Malhotra <78301665+KanishkM08@users.noreply.github.com>
2022-03-21 21:49:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client } = require("@notionhq/client");
+require("dotenv").config();
+
+const key = process.env.NOTION_API_KEY;
+const databaseID = process.env.NOTION_API_DATABASE;
+
+const notion = new Client({
+    auth: key
+});
+
+
+exports.getDatabase = async function () {
+    const response = await notion.databases.query({ database_id: databaseID });
+
+    const responseResults = response.results.map((page) => {
+            return {
+                id: page.id,
+                name: page.properties["Name"]["title"][0].plain_text,
+                present: page.properties["Present #"]["rich_text"][0].plain_text,
+                plan: page.properties["Payment Plan Type"]["rich_text"][0].plain_text,
+                duration: page.properties["Duration"]["rich_text"][0].plain_text
+            }
+    });
+
+    return responseResults;
+};
